2a4ac705d16977fe97a1052d131ebf7659d581f2
Bryan Yang <byang223@gmail.com>
2025-03-06 07:32:48 +0000

    subdecks based on new hsk levels

--- anki.py
+++ anki.py
@@ -8,7 +8,7 @@ with open(_filename, 'r') as file:
 
 my_model = genanki.Model(
   1607392319,
-  'Simple Model',
+  'Chinese Vocab',
   fields=[
     {'name': 'Vocab'},
     {'name': 'Answer'},
@@ -21,24 +21,31 @@ my_model = genanki.Model(
     },
   ])
 
-my_deck = genanki.Deck(
-  2059400110,
-  'Traditional Chinese')
+decks = []
+deck_id = 2059400110
+for i in range (1, 8):
+    decks.append(genanki.Deck(
+      2059400110 + i,
+      f'traditional_chinese::level{i}'))
 
 for e in data:
-    try:
-        tradChar = HanziConv.toTraditional(e['simplified'])
-    except:
-        print(e)
-        exit(1)
+    tradChar = HanziConv.toTraditional(e['simplified'])
     forms = []
     for form in e['forms']:
         meanings = '\n'.join(form['meanings'])
         forms.append(f'{form['transcriptions']['pinyin']}\n{meanings}')
+    level = -1
+    for l in e['level']:
+        if 'new' in l:
+            # example "new-1", map new-1 to 0
+            level = int(l[4:5]) - 1
+    if level == -1:
+        continue
     answer = '\n\n'.join(forms)
     my_note = genanki.Note(
         model=my_model,
         fields=[tradChar, answer])
-    my_deck.add_note(my_note)
+    decks[level].add_note(my_note)
 
-genanki.Package(my_deck).write_to_file('chinese.apkg')
+for i in range(len(decks)):
+  genanki.Package(decks[i]).write_to_file(f'chinese_{i+1}.apkg')
